47c7368e7899dfc1873248fed1577c0deea240a8
igorcrexito <igorcrexito@90ce6932-4c73-3b66-7c1f-8e47eba8c12c>
2013-10-16 15:47:58 +0000

    Ajuste das classes de produtos e transacoes

--- OticaKaka/src/utils/Produto.java
+++ OticaKaka/src/utils/Produto.java
@@ -10,4 +10,48 @@ package utils;
  */
 public class Produto {
     
+    private String codigo;
+    private String nome;
+    private double precoPorUnidade;
+    private int quantidadeEstoque;
+
+    public Produto(String codigo, String nome, double precoPorUnidade, int quantidadeEstoque) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.precoPorUnidade = precoPorUnidade;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrecoPorUnidade() {
+        return precoPorUnidade;
+    }
+
+    public void setPrecoPorUnidade(double precoPorUnidade) {
+        this.precoPorUnidade = precoPorUnidade;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+    
 }
