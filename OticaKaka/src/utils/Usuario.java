a9bf82952b28684f4a9d2b9b78cde6288d48f993
igorcrexito <igorcrexito@90ce6932-4c73-3b66-7c1f-8e47eba8c12c>
2013-10-17 15:14:34 +0000

    Adição da parte de login

---
+++ OticaKaka/src/utils/Usuario.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Igor
+ */
+public class Usuario {
+    
+    private String login;
+    private String senha;
+
+    public Usuario(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }   
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
