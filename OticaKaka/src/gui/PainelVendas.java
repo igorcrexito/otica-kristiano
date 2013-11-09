d4dab88f132304f926e268648afdcde40e2069ed
igorcrexito <igorcrexito@90ce6932-4c73-3b66-7c1f-8e47eba8c12c>
2013-11-09 18:37:25 +0000

    Done !

--- OticaKaka/src/gui/PainelVendas.java
+++ OticaKaka/src/gui/PainelVendas.java
@@ -675,7 +675,7 @@ public class PainelVendas extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(barraCliente, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(abasVendas, javax.swing.GroupLayout.DEFAULT_SIZE, 509, Short.MAX_VALUE)
+            .addComponent(abasVendas)
         );
     }// </editor-fold>//GEN-END:initComponents
 
